3c77028e1c330abc4b593f5451a2f95b8f66279a
KknanxX <72762570+KknanxX@users.noreply.github.com>
2024-07-09 18:53:09 -0700

    Initial Commit

---
+++ RadioPlayerApp/RadioPlayerAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  RadioPlayerAppApp.swift
+//  RadioPlayerApp
+//
+//  Created by KKNANXX on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RadioPlayerAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
